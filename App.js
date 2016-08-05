6dec3ff71c84c3d892306dc4dc4b004aac07082b
Dennis Hernández <dennishernandezvargas@gmail.com>
2016-08-05 10:41:48 -0600

    using mount, unmount, willmount

--- App.js
+++ App.js
@@ -6,24 +6,48 @@ import tags from 'hyperscript-helpers';
 const {div, h1, input, button} = tags(h);
 
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {val: 0};
+        this.update = this.update.bind(this);
+    }
+    update() {
+        this.setState({
+            val: this.state.val + 1
+        })
+    }
+    componentWillMount() {
+        console.log("mounting")
+    }
     render(){
-        return <Button>I <Heart/> React</Button>        
+        console.log("rendering!")
+        return button({onClick: this.update}, [this.state.val])
+    }
+    componentDidMount() {
+        console.log("mounted")
+    }
+    componentWillUnmount() {
+        console.log("bye!")
     }
 }
 
-class Button extends React.Component {
+class Wrapper extends React.Component {
+    constructor() {
+        super();
+    }
+    mount() {
+        ReactDOM.render(<App />, document.getElementById('a'))
+    }
+    unmount() {
+        ReactDOM.unmountComponentAtNode(document.getElementById('a'))
+    }
     render() {
-        return button([this.props.children])
+        return div([
+            button({onClick: this.mount.bind(this)}, ["Mount"]),
+            button({onClick: this.unmount.bind(this)}, ["Unmount"]),
+            div({id: 'a'})
+        ])
     }
 }
 
-const Heart = () => <span>♡</span>
-
-
-ReactDOM.render(
-    <App />,
-    document.getElementById('app')
-);
-
-
-export default App
+export default Wrapper
