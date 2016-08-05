53c5aa7012d0ce8d93db10911b5983abc8f3a761
Dennis Hernández <dennishernandezvargas@gmail.com>
2016-08-05 14:44:18 -0600

    Build a JSX Live Compiler

--- App.js
+++ App.js
@@ -3,44 +3,47 @@ import ReactDOM from 'react-dom';
 import h from 'react-hyperscript';
 import tags from 'hyperscript-helpers';
 
-const {div, h1, input, button, label, table, tbody, tr, td} = tags(h);
+const {div, h1, input, button, label, table, tbody, tr, td, header, textarea, pre} = tags(h);
 
 class App extends React.Component {
     constructor(){
         super();
-        this.state = {data: [
-            {id: 1, name: "Simon Bailey"},
-            {id: 2, name: "Thomas Burleson"},
-            {id: 3, name: "Will Button"},
-            {id: 4, name: "Ben Clinkinbeard"},
-            {id: 5, name: "Kent Dodds"},
-            {id: 6, name: "Trevor Ewen"},
-            {id: 7, name: "Aaron Frost"},
-            {id: 8, name: "Joel Hooks"},
-            {id: 9, name: "Jafar Husain"},
-            {id: 10, name: "Tim Kindberg"},
-            {id: 11, name: "John Lindquist"},
-            {id: 12, name: "Joe Maddalone"},
-            {id: 13, name: "Tyler McGinnis"},
-            {id: 14, name: "Scott Moss"},
-            {id: 15, name: "Robert Penner"},
-            {id: 16, name: "Keith Peters"},
-            {id: 17, name: "Lukas Ruebbelke"},
-            {id: 18, name: "Brett Shollenberger"}
-        ]}
+        this.state = {
+            input: '/* add jsx here */',
+            output: '',
+            err: ''
+        }
+        this.update = this.update.bind(this)
     }
-    render(){
-        let rows = this.state.data.map( person => {
-            return h(PersonRow, {key: person.id, data: person})
-        })
-        return table([tbody([rows])])
+    update(e) {
+        let code = e.target.value
+        try {
+            this.setState({
+                output: babel.transform(code, {
+                            stage: 0,
+                            loose: "all"
+                        }).code,
+                err: ''
+            })
+        }
+        catch(err) {
+          this.setState({
+            err: err.message
+          })
+        }
+    }
+    render() {
+        return div([
+            header([this.state.err]),
+            div(".container",[
+                textarea({
+                    onChange: this.update,
+                    defaultValue: this.state.input
+                }),
+                pre([this.state.output])
+            ])
+        ])
     }
 }
 
-const PersonRow = (props) => {
-    return tr([
-        td([props.data.id]),
-        td([props.data.name])
-    ])
-}
 export default App
