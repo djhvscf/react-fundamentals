d29a2b1a9feac08caeff95a1f83050817e017b47
Dennis Hernández <dennishernandezvargas@gmail.com>
2016-08-04 21:19:36 -0600

    Using ref

--- App.js
+++ App.js
@@ -3,23 +3,50 @@ import ReactDOM from 'react-dom';
 import h from 'react-hyperscript';
 import tags from 'hyperscript-helpers';
 
-const {div, h1} = tags(h);
+const {div, h1, input} = tags(h);
 
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            red: 0,
+            green: 0,
+            blue: 0
+        }
+        this.update = this.update.bind(this);
+    }
+    update(e) {
+        this.setState({
+            red: ReactDOM.findDOMNode(this.refs.red).value,
+            green: ReactDOM.findDOMNode(this.refs.green).value,
+            blue: ReactDOM.findDOMNode(this.refs.blue).value
+        })
+    }
     render(){
-        const style = {border: "1px solid black"};
-        let txt = this.props.txt
-        return div({style}, [h1([txt])])
+        //const style = {border: "1px solid black"};
+        //let txt = this.state.txt
+        //return div({style}, [Widget({onChange: this.update}), h1([txt])])
+        return (
+            <div>
+                {this.state.txt}
+                <hr/>
+                <Slider ref="red" update={this.update} />
+                {this.state.red}
+                <br/>
+                <Slider ref="green" update={this.update} />
+                {this.state.green}
+                <br/>
+                <Slider ref="blue"update={this.update} />
+                {this.state.blue}
+            </div>
+        )
     }
 }
 
-App.propTypes = {
-    txt: React.PropTypes.string,
-    cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-    txt: 'this is the default txt'
+class Slider extends React.Component {
+    render() {
+        return input({onChange: this.props.update, type: "range", min:"0", max:"255"})
+    }
 }
 
 ReactDOM.render(
